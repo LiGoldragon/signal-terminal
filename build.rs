2faad3a8787c962a5555aa2abf5e2721661e7925
li <li@goldragon.criome.net>
2026-07-07 09:35:13 +0200

    signal-terminal: port contract to TrueSchema stack

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@ use schema_rust::build::ContractCrateBuild;
 fn main() {
     ContractCrateBuild::from_environment(
         "signal-terminal",
-        "0.2.1",
+        "0.2.3",
         "SIGNAL_TERMINAL_UPDATE_SCHEMA_ARTIFACTS",
     )
     .expect_fresh();
